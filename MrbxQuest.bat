7d3eef43a91a66ce8f8fa185b5d792baefdb88df
Enzo <enzocapitanivieira@gmail.com>
2026-07-13 10:59:39 -0300

    Refactor: Leve refatorada no batch

--- MrbxQuest.bat
+++ MrbxQuest.bat
@@ -1,9 +1,14 @@
 echo off
 
+ECHO Tentando compilação...
+
 gcc main.c -o main.exe -lwinmm
 
 if %ERRORLEVEL% EQU 0 (
+    ECHO =================================
     ECHO Compilado com sucesso!
+    ECHO Incializando jogo.
+    ECHO =================================
     start main.exe
 ) else (
     ECHO ERRO DE COMPILAÇÂO! Veja as mensagens acima.
